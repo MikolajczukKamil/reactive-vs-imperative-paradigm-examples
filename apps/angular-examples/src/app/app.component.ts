3821cb14435c59e288f70588059fa164ef3a55a0
Kamil Mikołajczuk <k.mikolajczuk123@gmail.com>
2023-09-28 13:30:56 +0200

    chore: Przygotowanie widoku filtrowania list

--- apps/angular-examples/src/app/app.component.ts
+++ apps/angular-examples/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component }          from '@angular/core';
 import { MatTabsModule }      from '@angular/material/tabs'
 
+import { ListWithFiltersComponent } from '../list-with-filters'
+
 @Component({
   standalone: true,
-  imports: [ MatTabsModule ],
+  imports: [ MatTabsModule, ListWithFiltersComponent ],
   selector: 'reactive-vs-imperative-paradigm-examples-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
