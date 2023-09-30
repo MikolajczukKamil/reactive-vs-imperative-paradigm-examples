6ebf2aca8783802ebb7ce607fa03dd5eca95cea0
Kamil Mikołajczuk <k.mikolajczuk123@gmail.com>
2023-09-30 15:30:38 +0200

    chore: Przygotowanie widoku filtrowania list 2

--- apps/angular-examples/src/list-with-filters/etfs/etf.service.ts
+++ apps/angular-examples/src/list-with-filters/etfs/etf.service.ts
@@ -1,4 +1,5 @@
 import { Injectable }                 from '@angular/core'
+import { Sort }                       from '@angular/material/sort'
 import { delay, map, Observable, of } from 'rxjs'
 
 import allEtfs from './etfs.json'
@@ -21,9 +22,29 @@ export interface Filters {
 export class EtfService {
   private readonly all$: Observable<Etf[]> = of(allEtfs.filter(e => e.currency !== '—'))
   
-  public getEtfList(page: number, pageSize: number, filters: Filters = {}): Observable<Etf[]> {
+  public getEtfList(page: number, pageSize: number, filters: Filters = {}, sort?: Sort): Observable<Etf[]> {
     return this.all$.pipe(
       map((all) => all.filter(el => this.filter(el, filters))),
+      map((all) => {
+        if (sort?.active) {
+          [ ...all ].sort((a, b) => {
+            const av = a[sort.active as keyof Etf]
+            const bv = b[sort.active as keyof Etf]
+            
+            let res = 0
+            
+            if (typeof av === 'string') {
+              res = av.localeCompare(bv as string)
+            } else if (typeof av === 'number') {
+              res = (bv as number) - av
+            }
+            
+            return res * (sort.direction === 'desc' ? -1 : 1)
+          })
+        }
+        
+        return all
+      }),
       map((all) => all.slice((page - 1) * pageSize, page * pageSize)),
       delay(3000 * Math.random()),
     )
--- apps/angular-examples/src/list-with-filters/list-with-filters.component.ts
+++ apps/angular-examples/src/list-with-filters/list-with-filters.component.ts
@@ -1,10 +1,24 @@
-import { AsyncPipe, NgIf }      from '@angular/common'
-import { Component, inject }    from '@angular/core'
-import { MatProgressBarModule } from '@angular/material/progress-bar'
-import { MatTableModule }       from '@angular/material/table'
+import { AsyncPipe, NgIf }                                       from '@angular/common'
+import { Component, inject }                                     from '@angular/core'
+import { MatCardModule }                                         from '@angular/material/card'
+import { MatIconModule }                                         from '@angular/material/icon'
+import { MatInputModule }                                        from '@angular/material/input'
+import {
+  MatProgressBarModule,
+}                                                                from '@angular/material/progress-bar'
+import { MatSelectModule }                                       from '@angular/material/select'
+import { MatSortModule, Sort }                                   from '@angular/material/sort'
+import { MatTableModule }                                        from '@angular/material/table'
+import { BehaviorSubject, combineLatest, Observable, startWith } from 'rxjs'
 
 import { Etf, EtfService } from './etfs'
 
+
+interface Ctx {
+  loading: boolean
+  error: boolean
+  data: Etf[] | null
+}
 
 @Component({
   standalone: true,
@@ -13,6 +27,11 @@ import { Etf, EtfService } from './etfs'
     AsyncPipe,
     NgIf,
     MatProgressBarModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    MatSelectModule,
+    MatSortModule,
   ],
   selector: 'list-with-filters',
   templateUrl: './list-with-filters.component.html',
@@ -21,10 +40,20 @@ import { Etf, EtfService } from './etfs'
 export class ListWithFiltersComponent {
   private readonly etfs = inject(EtfService)
   
-  protected readonly dataSource$ = this.etfs.getEtfList(1, 25, {})
+  protected readonly loading$ = new BehaviorSubject(false)
+  protected readonly error$ = new BehaviorSubject(false)
+  protected readonly sort$ = new BehaviorSubject<Sort | null>(null)
+  
+  protected readonly data$ = this.etfs.getEtfList(1, 25, {})
   protected readonly displayedColumns = [ 'name', 'price', 'currency' ]
   
+  protected readonly ctx$: Observable<Ctx> = combineLatest({
+    loading: this.loading$.asObservable(),
+    error: this.error$.asObservable(),
+    data: this.data$.pipe(startWith(null)),
+  })
+  
   element(el: unknown): Etf {
-    return el as Etf;
+    return el as Etf
   }
 }
