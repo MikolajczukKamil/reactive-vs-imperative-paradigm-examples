9c9263390adb235a5e39e5df75af6b35fd67ed7f
Kamil Mikołajczuk <k.mikolajczuk123@gmail.com>
2024-06-03 22:03:57 +0200

    chore: react 2

--- apps/react-examples/src/list-with-filters/ListWithFilters.tsx
+++ apps/react-examples/src/list-with-filters/ListWithFilters.tsx
@@ -5,17 +5,33 @@ import {
   FormControl,
   InputLabel,
   MenuItem,
+  Paper,
   Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TableSortLabel,
   TextField,
 }                            from '@mui/material'
 import { SelectChangeEvent } from '@mui/material/Select/SelectInput'
-import { useState }          from 'react'
+import { SortDirection }                     from '@mui/material/TableCell/TableCell'
+import { useState, MouseEvent, ChangeEvent } from 'react'
 
 
 interface Currency {
   code: string
   name: string
 }
+
+interface Sort {
+  active: string;
+  direction: SortDirection;
+}
+
 
 export function ListWithFilters() {
   const [ currency, setCurrency ] = useState<string | null>(null)
@@ -35,6 +51,48 @@ export function ListWithFilters() {
   function handleClear(): void {
     setCurrency(null)
   }
+  
+  const [ sort, setSort ] = useState<Sort>({ active: '', direction: false })
+  const [ page, setPage ] = useState(0)
+  const [ rowsPerPage, setRowsPerPage ] = useState(5)
+  
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage)
+  }
+  
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10))
+    setPage(0)
+  }
+  
+  const rows: any[] = []
+  
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0
+
+  
+  const headCells = [
+    {
+      name: 'name',
+      label: 'Nazwa',
+    }, {
+      name: 'price',
+      label: 'Cena',
+    }, {
+      name: 'currency',
+      label: 'Waluta',
+    },
+  ]
+  
+  const createSortHandler =
+    (active: string) => (event: MouseEvent<unknown>) => {
+      console.log(event);
+    
+      // setSort({
+      //   active, direction: event.target.value
+      // })
+    }
   
   return (
     <Box sx={ { maxWidth: 1000, margin: 'auto' } }>
@@ -63,6 +121,66 @@ export function ListWithFilters() {
           <Button onClick={ handleClear }>Wyczyść i przeładuj</Button>
         </Card>
       </form>
+      
+      <Paper sx={ { width: '100%', mb: 2 } }>
+        <TableContainer>
+          <Table sx={ { minWidth: 750 } }>
+            <TableHead>
+              <TableRow>
+                { headCells.map((headCell) => (
+                  <TableCell
+                    key={ headCell.name }
+                    sortDirection={ sort.active === headCell.name ? sort.direction : false }
+                  >
+                    <TableSortLabel
+                      active={ sort.active === headCell.name }
+                      direction={ sort.active === headCell.name && sort.direction === 'desc' ? 'desc' : 'asc' }
+                      onClick={ createSortHandler(headCell.name) }
+                    >
+                      { headCell.label }
+                      {/*{ orderBy === headCell.id ? (*/}
+                      {/*  <Box component="span" sx={ visuallyHidden }>*/}
+                      {/*    { order === 'desc'*/}
+                      {/*      ? 'sorted descending'*/}
+                      {/*      : 'sorted ascending' }*/}
+                      {/*  </Box>*/}
+                      {/*) : null }*/}
+                    </TableSortLabel>
+                  </TableCell>
+                )) }
+              </TableRow>
+            </TableHead>
+            
+            <TableBody>
+              { rows.map((row, index) => (
+                <TableRow hover role="checkbox" key={ row.id }>
+                  <TableCell component="th" scope="row" padding="none">
+                    { row.name }
+                  </TableCell>
+                  <TableCell align="right">{ row.calories }</TableCell>
+                  <TableCell align="right">{ row.fat }</TableCell>
+                  <TableCell align="right">{ row.carbs }</TableCell>
+                  <TableCell align="right">{ row.protein }</TableCell>
+                </TableRow>
+              )) }
+              { emptyRows > 0 && (
+                <TableRow style={ { height: 53 * emptyRows } }>
+                  <TableCell colSpan={ 5 } />
+                </TableRow>
+              ) }
+            </TableBody>
+          </Table>
+        </TableContainer>
+        
+        <TablePagination
+          rowsPerPageOptions={ [ 5, 10, 25 ] }
+          count={ rows.length }
+          rowsPerPage={ rowsPerPage }
+          page={ page }
+          onPageChange={ handleChangePage }
+          onRowsPerPageChange={ handleChangeRowsPerPage }
+        />
+      </Paper>
     </Box>
   )
 }
