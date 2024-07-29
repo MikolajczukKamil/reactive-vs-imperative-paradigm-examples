747d141726bf084710c48150ea2ba158605b7f16
Kamil Mikołajczuk <k.mikolajczuk123@gmail.com>
2024-07-29 13:48:39 +0200

    chore: init plain

--- apps/plain-examples/src/app/nx-welcome.tsx
+++ apps/plain-examples/src/app/nx-welcome.tsx
@@ -5,16 +5,21 @@ export interface NxWelcomeComponentProps {
   title: string;
 }
 
-class NxWelcomeComponent extends CustomElement implements NxWelcomeComponentProps {
-  get title(): string {
-    return this.attribute('title') || ''
+class NxWelcomeComponent extends CustomElement<NxWelcomeComponentProps> implements NxWelcomeComponentProps {
+  public static observedAttributes = [ 'title' ] as const
+  
+  public set title(title: string) {
+    this.p.innerText = `Hello ${ title }!`
   }
   
+  public get title(): string {
+    return this.getAttribute('title') || ''
+  }
+  
+  private readonly p = document.createElement('p')
+  
   protected override connectedCallback(): void {
-    const p = document.createElement('p')
-    p.innerText = `Hello ${ this.title }!`
-    
-    this.appendChild(p)
+    this.appendChild(this.p)
   }
 }
 
--- apps/plain-examples/src/app/utils.ts
+++ apps/plain-examples/src/app/utils.ts
@@ -1,9 +1,13 @@
 import { FunctionComponent } from 'react'
 
 
-export abstract class CustomElement extends HTMLElement {
-  protected attribute(name: string): undefined | string {
-    return this.attributes.getNamedItem(name)?.value
+export abstract class CustomElement<Props = {}> extends HTMLElement {
+  protected attributeChangedCallback(name: keyof Props, _: null | string, value: Props[keyof Props]): void {
+    try {
+      (this as unknown as Props)[name] = value;
+    } catch (e) {
+      console.error('CustomElement.attributeChangedCallback', { name, value }, e);
+    }
   }
   
   protected abstract connectedCallback(): void;
