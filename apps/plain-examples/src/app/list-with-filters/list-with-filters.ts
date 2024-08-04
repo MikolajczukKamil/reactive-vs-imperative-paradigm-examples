39186e3b9abcb2379d27ea9ed907b31802d5a30d
Kamil Mikołajczuk <k.mikolajczuk123@gmail.com>
2024-08-04 16:07:18 +0200

    chore: loading

--- apps/plain-examples/src/app/list-with-filters/list-with-filters.ts
+++ apps/plain-examples/src/app/list-with-filters/list-with-filters.ts
@@ -1,7 +1,8 @@
 // @ts-ignore
-import { initMDB, Tab } from 'mdb-ui-kit'
+import { initMDB, Ripple } from 'mdb-ui-kit'
 
 import { CustomElement, defineComponent } from '../utils'
+import { Etf, injectEtfsService }         from './etfs'
 
 import componentTemplate from './list-with-filters.html'
 import './list-with-filters.scss'
@@ -14,6 +15,10 @@ export interface AppComponentProps {
 class ListWithFilterComponent extends CustomElement<AppComponentProps> implements AppComponentProps {
   public static observedAttributes = [ 'title' ] as const
   
+  public isLoading = false
+  public instruments: Etf[] = []
+  private etfService = injectEtfsService()
+  
   public set title(title: string) {
     this.template.innerText = `Hello ${ title }!`
   }
@@ -24,6 +29,56 @@ class ListWithFilterComponent extends CustomElement<AppComponentProps> implement
   
   public constructor() { super(componentTemplate) }
   
+  protected override connectedCallback(): void {
+    super.connectedCallback()
+    
+    this.reloadInstrument()
+    
+    initMDB({ Ripple })
+  }
+  
+  private reloadInstrument(): void {
+    this.isLoading = true
+    this.updateLoading()
+    
+    this.etfService.getEtfList(1, 10)
+        .then((page) => {
+          this.isLoading = false
+          this.instruments = page.items
+          
+          this.updateList()
+          this.updateLoading()
+        })
+        .catch((error) => {
+          console.error(error)
+          
+          this.isLoading = false
+          this.updateLoading()
+        })
+  }
+  
+  private updateLoading(): void {
+    const loadingElement = this.querySelector<HTMLElement>('#loading')!
+    
+    loadingElement.style.display = this.isLoading ? 'block' : 'none'
+  }
+  
+  private updateList(): void {
+    const instrumentsList = this.querySelector<HTMLElement>('#instruments-list')!
+    
+    console.log(this.instruments, instrumentsList)
+    
+    instrumentsList.replaceChildren(
+      ...this.instruments.map((instrument) => {
+        const cell = document.createElement('tr')
+        
+        cell.innerHTML = `<td>${ instrument.name }</td><td>${ instrument.price }</td><td>${ instrument.currency }</td>`
+        
+        return cell
+      }),
+    )
+    
+  }
 }
 
 export const ListWithFilters = defineComponent('list-with-filters', ListWithFilterComponent)
