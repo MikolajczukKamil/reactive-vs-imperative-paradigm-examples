25e965cd27eed930abdb0310f4fd7c0141665c72
Kamil Mikołajczuk <k.mikolajczuk123@gmail.com>
2024-08-11 01:17:05 +0200

    chore: migrations 3

--- apps/plain-examples/webpack.config.js
+++ apps/plain-examples/webpack.config.js
@@ -5,7 +5,7 @@ const { withReact } = require('@nx/react')
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
-  config.module.rules.push({
+  config?.module?.rules?.push({
     test: /\.html$/i,
     loader: 'html-loader',
   })
