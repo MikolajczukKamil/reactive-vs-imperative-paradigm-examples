ea50e769ad986d095a5c11f37876530344b68d4e
Kamil Mikołajczuk <k.mikolajczuk123@gmail.com>
2024-08-31 22:55:30 +0200

    api

--- apps/demo-data-be/src/app/app.controller.ts
+++ apps/demo-data-be/src/app/app.controller.ts
@@ -1,14 +1,34 @@
 import { Controller, Get, ParseFloatPipe, ParseIntPipe, Query } from '@nestjs/common';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags }         from '@nestjs/swagger';
 import { SortDirection, SortProperty }                          from '@org/common-lib';
 
-import { EtfService } from './etf.service';
+import { EtfPageEntity } from './etf-page.entity';
+import { EtfService }    from './etf.service';
 
 
+@ApiTags('ETF')
 @Controller()
 export class AppController {
   constructor(private readonly etfService: EtfService) {}
   
   @Get('etf')
+  @ApiOperation({ summary: 'ETF search' })
+  @ApiResponse({ status: 401, description: 'Unauthorized, access is denied' })
+  @ApiResponse({ status: 404, description: 'Not found' })
+  @ApiResponse({ type: EtfPageEntity, description: 'ETF search' })
+  @ApiQuery({ name: 'page', required: false, type: 'int', example: 2 })
+  @ApiQuery({ name: 'page-size', required: false, type: 'int', example: 10 })
+  @ApiQuery({ name: 'sort-property', required: false, type: String, example: 'price' })
+  @ApiQuery({ name: 'sort-direction', required: false, type: 'asc | desc', example: 'desc' })
+  @ApiQuery({ name: 'search', required: false, type: String, example: 10 })
+  @ApiQuery({
+    name: 'currency',
+    required: false,
+    type: 'EUR | USD | PLN | GBP | CHF',
+    example: 'USD'
+  })
+  @ApiQuery({ name: 'min-price', required: false, type: Number, example: 20 })
+  @ApiQuery({ name: 'max-price', required: false, type: Number, example: 50 })
   public async getData(
     @Query('page', ParseIntPipe) page: number,
     @Query('page-size', ParseIntPipe) pageSize: number,
@@ -18,7 +38,7 @@ export class AppController {
     @Query('currency') currency?: string,
     @Query('min-price', new ParseFloatPipe({ optional: true })) minPrice?: number,
     @Query('max-price', new ParseFloatPipe({ optional: true })) maxPrice?: number
-  ) {
+  ): Promise<EtfPageEntity> {
     console.debug({
       page,
       pageSize,
--- apps/demo-data-be/src/main.ts
+++ apps/demo-data-be/src/main.ts
@@ -3,8 +3,9 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger }      from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { Logger }                         from '@nestjs/common';
+import { NestFactory }                    from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -14,6 +15,23 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 8888;
+  
+  const config = new DocumentBuilder()
+    .setTitle('ETF search')
+    .setDescription(
+      'API umożliwiające wyszukiwanie serwisów ETF pozwala użytkownikom na zdalne przeszukiwanie bazy danych funduszy ETF. ' +
+      'Z jego pomocą użytkownicy mogą wyszukiwać informacje na temat ETF-ów na podstawie różnych kryteriów, ' +
+      'takich jak nazwa funduszu, ticker, rynek docelowy, kategoria, opłaty, historyczne wyniki i inne wskaźniki finansowe. ' +
+      'API zwraca odpowiednie dane w formacie JSON, umożliwiając integrację z aplikacjami, platformami inwestycyjnymi lub narzędziami analitycznymi. ' +
+      'Dzięki temu, użytkownicy mogą łatwo znaleźć ETF-y spełniające ich specyficzne wymagania inwestycyjne.'
+    )
+    .setVersion('1.0.0')
+    .addTag('ETF')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${ port }/${ globalPrefix }`);
 }
